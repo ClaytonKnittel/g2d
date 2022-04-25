bc4a2f46bf0a1cdb23723fe601441083f4e38950
ClaytonKnittel <cknittel@wustl.edu>
2022-04-24 18:36:16 -0700

    arch_test

---
+++ src/arch_test/test_avx.s
@@ -0,0 +1,4 @@
+.global _test_avx
+
+_test_avx:
+	vaddpd ymm0, ymm1
---
+++ src/arch_test/test_empty.c
@@ -0,0 +1,11 @@
+
+/*
+ * An empty source file, used to test support for certain assembly instruction
+ * types.
+ */
+int
+main()
+{
+	return 0;
+}
+
