49ec9654b6d73c613513e41753c2b4528f080b37
claytonknittel <cknittel@wustl.edu>
2022-09-24 17:29:41 -0700

    move example code to examples dir

--- examples/test/test_basic.cc
+++ examples/test/test_basic.cc
@@ -21,7 +21,6 @@ static uint32_t f2i(float f) {
   return tmp.int_val;
 }
 
-/*
 int main() {
 #ifdef USE_METAL
   // call_test_ray();
@@ -66,4 +65,3 @@ int main() {
   gl_exit(&gl_ctx);
 #endif
 }
-*/
